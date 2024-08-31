3cfcb417fc585ff30cdcc91b93864c3d8e21533e
Hayane23 <hayane.silva23@gmail.com>
2024-08-31 00:11:22 -0300

    footer.

--- src/components/layouts/Footer.vue
+++ src/components/layouts/Footer.vue
@@ -1,28 +1,35 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-dark">
+    <nav class="footer navbar-expand-lg navbar-dark">
         <div class="container">
-            <a class="navbar-brand" href="#">
-                <img src="../../assets/img/logo.png" alt="ALLZ" width="134" height="" class="d-inline-block align-text-top">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Inicio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Quem Somos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Produtos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Fale Conosco</a>
-                    </li>
-                    <button class="btn btn-outline-light btn-sm mx-2" type="button">Começar Projeto</button>
-                </ul>
+            <div>
+                <div class="d-flex">
+                    <a class="navbar-brand" href="#">
+                        <img src="../../assets/img/iconblue.png" alt="ALLZ" width="134" height="" class="d-inline-block align-text-top">
+                    </a>
+                    <div>
+                        <div>
+                            <p>CONTATO</p>
+                            <div class="d-flex align-items-center">
+                                <img class="pe-1" src="../../assets/img/iconemail.png" alt="Icone e-mail">
+                                <p class="mb-0">comercial@allztech.com.br</p>
+                            </div>
+                            <div class="d-flex align-items-start">
+                                <img class="pe-1" src="../../assets/img/iconpin.png" alt="Icone localização">
+                                <div>
+                                    <p>Rua Gomes de Carvalho 892 </br>
+                                        Conj. 76 a 78 – Vila Olímpia </br>
+                                        CEP: 04547-003
+                                    </p>
+                                </div>
+                            </div>
+                            <p>Política de Privacidade</p>
+                        </div>
+                        <div>
+                            <a href="http://" target="_blank" rel="noopener noreferrer"><img src="../../assets/img/iconinstagram.png" alt="Logotipo Instagram"></a>
+                        </div>
+                    </div>
+                </div>
+                <p class="fw-bold">@2024 ALLZ TECH</p>
             </div>
         </div>
     </nav>
@@ -35,7 +42,8 @@
 </script>
 
 <style>
-    .navbar {
-        background-color: #0149FD;
+    .footer {
+        background-color: #00050D;
+        color: #fff;
     }
 </style>
